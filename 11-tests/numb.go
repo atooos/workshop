8f98e626b98bb7a6e43658f01b18e35ce0ca2c65
Henri LEPIC <henrilepic@gmail.com>
2021-04-08 10:49:28 +0200

    add unit test

--- 11-tests/numb.go
+++ 11-tests/numb.go
@@ -1,9 +1,19 @@
 package myint
 
+import (
+	"errors"
+)
+
+var ErrTooLarge = errors.New("values too large")
+
 type MyInt int32
 
-func (i MyInt) Sub(n int) MyInt {
-	return i - MyInt(n)
+func (i MyInt) Sub(n int) (MyInt, error) {
+	res := int64(i) - int64(n)
+	if res < -2147483648 {
+		return 0, ErrTooLarge
+	}
+	return MyInt(res), nil
 }
 
 func (i MyInt) Add(n int) MyInt {
--- 11-tests/numb_test.go
+++ 11-tests/numb_test.go
@@ -1,1 +1,26 @@
 package myint
+
+import (
+	"testing"
+)
+
+func TestMyIntSub(t *testing.T) {
+	data := []struct {
+		title  string
+		init   MyInt
+		param  int
+		output int
+		err    error
+	}{
+		{"A", 1, 1, 0, nil},
+		{"B", -2147483648, 2, 0, ErrTooLarge},
+	}
+	for _, d := range data {
+		res, err := d.init.Sub(d.param)
+		t.Logf("res of sub is %v", res)
+		if res != MyInt(d.output) || d.err != err {
+			t.Errorf("for test %v got %v but is waiting for %v", d.title, res, d.output)
+		}
+	}
+
+}
